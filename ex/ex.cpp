f59f6e233bd1a21ce43df6df037062c24950d075
roman bokhonkovich <bokhonkovich2015@mail.ru>
2022-11-03 11:44:57 +0300

    calc not finished

---
+++ ex/ex.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+class calc
+{
+	double amount;
+public:
+	calc(double at) : amount(at)
+	{};
+	calc() : amount(0)
+	{};
+	double fadd(calc t1, calc t2)
+	{
+		calc temp;
+		amount = t1.amount + t2.amount;
+		return amount;
+	}
+	double fsub(calc t1, calc t2)
+	{
+		calc temp;
+		amount = t1.amount - t2.amount;
+		return amount;
+	}
+	double fmul(calc t1, calc t2)
+	{
+		amount = t1.amount * t2.amount;
+		return amount;
+	}
+	double fdiv(calc t1, calc t2)
+	{
+		amount = t1.amount / t2.amount;
+		return amount;
+	}
+};
+
+int main()
+{
+	calc res;
+    calc t1(5);
+	calc t2(10);
+	cout << res.fdiv(t1,t2);
+}
